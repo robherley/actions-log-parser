3e17add3d82f1b65e20d435474f467d20f738da7
Rob Herley <robherley13@gmail.com>
2025-10-02 21:08:14 -0400

    repo rename

--- demo/src/components/Layout.tsx
+++ demo/src/components/Layout.tsx
@@ -13,12 +13,12 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
     <Header>
       <Header.Item>
         <Header.Link
-          href="https://github.com/robherley/actionslogs-js"
+          href="https://github.com/robherley/actions-log-parser"
           sx={{ color: "fg.muted" }}
         >
           <CodeIcon size={16} />
           <Text as="strong" sx={{ marginLeft: themeGet("space.2") }}>
-            robherley/actionslogs-js
+            robherley/actions-log-parser
           </Text>
         </Header.Link>
       </Header.Item>
